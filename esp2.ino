5ceb6019856be37daa7a0f1f7ee2bde06dd998b4
Defta Andrei-Robert <andreideftarr@gmail.com>
2025-10-27 16:05:58 +0200

    Integrate MQTT and sensor data handling
    Added MQTT functionality and sensor readings.

--- esp2.ino
+++ esp2.ino
@@ -1,59 +1,88 @@
 #include <WiFi.h>
-#include <WebServer.h>
-#include <LittleFS.h>
-
-
-const char* ssid = "Orange-b39kTz-2G";           // <- SSID-ul rețelei tale
-const char* password = "K22ZyGy994x9AC6FUN";     // <- Parola rețelei tale
-
-WebServer server(80);
-
-void handleRoot() {
-  if (LittleFS.exists("/index.html")) {
-    File file = LittleFS.open("/index.html", "r");
-    server.streamFile(file, "text/html");
-    file.close();
-  } else {
-    server.send(500, "text/plain", "Fisierul index.html nu a fost gasit.");
-  }
-}
-
-void handleLedOn() {
-  digitalWrite(2, HIGH);
-  server.send(200, "text/plain", "LED aprins");
-}
-
-void handleLedOff() {
-  digitalWrite(2, LOW);
-  server.send(200, "text/plain", "LED stins");
-}
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+// 2) Maparea pinilor (senzori)
+#define ONE_WIRE_BUS 15
+#define SOIL_MOISTURE_PIN 32
+#define SOIL_MOISTURE_PIN_2 35
+#define MQ1_PIN 33 // Nivel 1 jos
+#define MQ2_PIN 34 //D34 Nivel 2 mijloc
+#define MQ3_PIN 39 // nivel 3 cel mai sus
+// 3) Config Wi-Fi
+const char* ssid = "DIGI-6yNU";
+const char* password = "Tj3ug9wTPw";
+//4) MQTT ADD
+const char* MQTT_HOST ="192.168.1.137"; // IP de pe Rasbbery
+const int MQTT_PORT = 1883;
+const char* MQTT_USER = ""; // ex. "espuser"
+const char* MQTT_PASS = ""; // ex. "parola"
+93
+const char* MQTT_CLIENT_ID = "esp32-sensor-01";
+const char* TOPIC_AVAIL = "automatizare/esp32/availability";
+const char* TOPIC_STATE = "automatizare/esp32/senzori/state";
+const char* TOPIC_CMD = "automatizare/esp32/cmd";
+WiFiServer server(80);
+WiFiClient net ;
+PubSubClient mqtt(net); // Enter MQTT ADD
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
-  }
-  Serial.println("\nConectat la IP: " + WiFi.localIP().toString());
-
-  if (!LittleFS.begin()) {
-    Serial.println("LittleFS nu a putut fi inițializat!");
-    return;
-  }
-
-  pinMode(2, OUTPUT);
-
-  server.on("/", handleRoot);
-  server.on("/led/on", handleLedOn);
-  server.on("/led/off", handleLedOff);
-  server.onNotFound([]() {
-    server.send(404, "text/plain", "Pagina nu exista");
-  });
-
-  server.begin();
+Serial.begin(115200);
+WiFi.begin(ssid, password);
+Serial.print("Conectare la WiFi");
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
 }
-
+Serial.println("\nConectat la WiFi!");
+Serial.print("IP ESP32: ");
+Serial.println(WiFi.localIP());
+server.begin();
+sensors.begin();
+pinMode(SOIL_MOISTURE_PIN, INPUT);
+pinMode(SOIL_MOISTURE_PIN_2, INPUT);
+}
 void loop() {
-  server.handleClient();
-}
+sensors.requestTemperatures();
+float temperatureC = sensors.getTempCByIndex(0);
+int raw1 = analogRead(SOIL_MOISTURE_PIN);
+int raw2 = analogRead(SOIL_MOISTURE_PIN_2);
+int valoare1 = analogRead(MQ1_PIN);
+int valoare2 = analogRead(MQ2_PIN);
+int valoare3 = analogRead(MQ3_PIN);
+float moisture1 = map(raw1, 4095, 0, 0, 100);
+94
+float moisture2 = map(raw2, 4095, 0, 0, 100);
+float meanMoisture = (moisture1 + moisture2) / 2;
+float Co2=(valoare1 + valoare2 + valoare3) / 3 ;
+// Afiseaza valorile in Serial Monitor
+Serial.print("Temperatura: ");
+Serial.print(temperatureC);
+Serial.print(" °C, Umiditate1: ");
+Serial.print(moisture1);
+Serial.print("%, Umiditate2: ");
+Serial.print(moisture2);
+Serial.print("%, Medie: ");
+Serial.print(meanMoisture);
+Serial.println("%");
+Serial.println("Senzor 1 CO2 nivel 1 ");Serial.println(valoare1);
+Serial.println("Senzor 2 CO2 nivel 2 ");Serial.println(valoare2);
+Serial.println("Senzor 3 Co2 nivel 3");Serial.println(valoare3);
+Serial.println("Valoare Medie Co2 :") ; Serial.println(Co2);
+WiFiClient client = server.available();
+if (client) {
+client.setTimeout(2000); // Timeout pentru client
+StaticJsonDocument<256> doc;
+doc["temperature"] = temperatureC;
+doc["meanMoisture"] = meanMoisture;
+String jsonString;
+serializeJson(doc, jsonString);
+client.println("HTTP/1.1 200 OK");
+client.println("Content-Type: application/json");
+client.println("Connection: close");
+client.println();
+client.println(jsonString);
+client.stop();}
+delay(1000)}
